4e7328a8a7fee640794895b0a864ebf3ca9c8110
victordruc <druc92@gmail.com>
2021-08-29 14:56:03 +0300

    editRoute

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,19 +1,19 @@
-import {useState, useContext} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Popover from '@material-ui/core/Popover';
-import IconButton from '@material-ui/core/IconButton';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import Badge from '@material-ui/core/Badge';
-import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Money from '../ui/Money';
-import {CartContext} from '../../state/HOCState'
+import { useState, useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Popover from "@material-ui/core/Popover";
+import IconButton from "@material-ui/core/IconButton";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Badge from "@material-ui/core/Badge";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Money from "../ui/Money";
+import { CartContext } from "../../state/HOCState";
 
 const useStyles = makeStyles((theme) => ({
   typography: {
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
   demo: {
     backgroundColor: theme.palette.background.paper,
   },
-}))
+}));
 
 const Cart = () => {
   const { state, dispatch } = useContext(CartContext);
@@ -30,49 +30,27 @@ const Cart = () => {
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
+    setAnchorEl(event.currentTarget);
   };
 
   const handleClose = () => {
-    setAnchorEl(null)
+    setAnchorEl(null);
   };
 
-  const open = Boolean(anchorEl)
-  const id = open ? 'simple-popover' : undefined
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
   return (
     <div>
-      {/* <IconButton
-        aria-label="cart"
-        aria-controls="menu-appbar"
-        aria-haspopup="true"
-        onClick={handleMenu}
+      <IconButton
+        aria-describedby={id}
+        variant="contained"
         color="inherit"
+        onClick={handleClick}
       >
-        <Badge badgeContent={state.count} color="secondary">
+        <Badge color="secondary" badgeContent={state.count}>
           <ShoppingCartIcon />
         </Badge>
-      </IconButton>
-      <StyledMenu
-        id="menu-appbar"
-        anchorEl={anchorEl}
-        keepMounted
-        open={open}
-        onClose={handleClose}
-      >
-        {state.cart.items.map((item, index) => (
-          <StyledMenuItem key={index} onClick={handleClose}>
-            <Avatar alt={item.name} src={item.imageUrls[0]} />
-            <Typography variant="subtitle1">{item.name}</Typography>
-          </StyledMenuItem>
-        ))}
-      </StyledMenu> */}
-
-
-<IconButton aria-describedby={id} variant="contained" color="inherit" onClick={handleClick}>
-            <Badge color="secondary" badgeContent={state.count}>
-              <ShoppingCartIcon/>
-            </Badge>
       </IconButton>
       <Popover
         id={id}
@@ -80,45 +58,71 @@ const Cart = () => {
         anchorEl={anchorEl}
         onClose={handleClose}
         anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
+          vertical: "bottom",
+          horizontal: "right",
         }}
         transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
+          vertical: "top",
+          horizontal: "right",
         }}
-      > 
-        {state.cart.items.length?<div className={classes.demo}>
-            <List>
-              {state.cart.items.map((product,index)=>
-                <ListItem key={index}>
+      >
+        {state.cart.items.length ? (
+          <div className={classes.demo}>
+            <List component="nav">
+              {state.cart.items.map((product, index) => (
+                <ListItem button component="a" key={index}>
                   <ListItemAvatar>
-                    <Avatar alt={product.name} src={product.imageUrls[0]}/>
+                    <Avatar alt={product.name} src={product.imageUrls[0]} />
                   </ListItemAvatar>
                   <ListItemText
                     primary={product.name}
                     secondary={
                       <>
-                     {product.price.discount?
-                    <Money type="std" price={{amount:product.price.discount.amount, value:product.price.discount.currency}}>Price: </Money>:
-                    <Money type="std" price={{amount:product.price.standard.amount, value:product.price.standard.currency}}>Price: </Money>}
-                    <span> QTY: {product.quantity}</span>
-                    </>
-                  }
+                        {product.price.discount ? (
+                          <Money
+                            type="std"
+                            price={{
+                              amount: product.price.discount.amount,
+                              value: product.price.discount.currency,
+                            }}
+                          >
+                            Price:{" "}
+                          </Money>
+                        ) : (
+                          <Money
+                            type="std"
+                            price={{
+                              amount: product.price.standard.amount,
+                              value: product.price.standard.currency,
+                            }}
+                          >
+                            Price:{" "}
+                          </Money>
+                        )}
+                        <span> QTY: {product.quantity}</span>
+                      </>
+                    }
                   />
                   <ListItemSecondaryAction>
-                    <IconButton edge="end" aria-label="delete" onClick={()=>dispatch({ type: "removeAllQuantity", id:product.id })}>
+                    <IconButton
+                      edge="end"
+                      aria-label="delete"
+                      onClick={() =>
+                        dispatch({ type: "removeAllQuantity", id: product.id })
+                      }
+                    >
                       <DeleteIcon />
                     </IconButton>
                   </ListItemSecondaryAction>
-                </ListItem>)}
+                </ListItem>
+              ))}
             </List>
-          </div>:<Typography
-          className={classes.typography}
-          color="primary"
-      >
-        Empty cart
-      </Typography>}
+          </div>
+        ) : (
+          <Typography className={classes.typography} color="primary">
+            Empty cart
+          </Typography>
+        )}
       </Popover>
     </div>
   );
